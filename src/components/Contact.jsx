1f20e6d4d1e0f16dbf5f7f2872893787dc82513c
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-02-05 23:36:27 +0800

    added node files and edited index.css

---
+++ src/components/Contact.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../stylesheets/contact.css'
+
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
---
+++ src/components/Services.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../stylesheets/services.css'
+
+const Services = () => {
+  return (
+    <div>Services</div>
+  )
+}
+
+export default Services
