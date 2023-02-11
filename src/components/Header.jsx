3591aa0177d75ae7f959a32cc7ed1e86d7920e85
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-02-11 14:03:03 +0800

    added header components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import HeaderButtons from './HeaderButtons'
+import HeaderSocials from './HeaderSocials'
+
 import '../stylesheets/header.css'
+
+// assets
+import homepic from '../assets/homepic.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className = 'container header__container'> 
+    <div className= 'header_text'>
+    <h5>Hello I'm</h5>
+      <h1>Ysabella Panghulan</h1>
+      <h5 className='text-light'>Fullstack Developer</h5>
+    </div>
+      <HeaderButtons />
+      <HeaderSocials />
+      <div className='pic'>
+        <img src={homepic} alt=''></img>
+      </div>
+
+      <a href='#contact' className='scroll_down'> Scroll Down</a>
+    </div>
   )
 }
 
---
+++ src/components/HeaderButtons.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeaderButtons = () => {
+  return (
+    <header>
+    <div className='buttons'>
+        <a href='' className='btn'>Download CV</a>
+        <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+    </div>
+    </header>
+  )
+}
+
+export default HeaderButtons
