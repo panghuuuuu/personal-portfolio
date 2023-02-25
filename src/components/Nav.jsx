aca60c2380552f7e689f59058c54edabc82498d3
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-02-26 03:07:23 +0800

    addeed and linked nav icons

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import '../stylesheets/nav.css'
-
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {BiBook} from 'react-icons/bi'
+import {RiServiceLine} from 'react-icons/ri'
+import {AiOutlineMessage} from 'react-icons/ai'
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+    <a href='#'><AiOutlineHome/></a>
+    <a href='#about'><AiOutlineUser /></a>
+    <a href='#experience'><BiBook /></a>
+    <a href='#services'><RiServiceLine /></a>
+    <a href='#contact'><AiOutlineMessage /></a>
+    </nav>
+
   )
 }
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,7 +3,7 @@ import '../stylesheets/services.css'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section id='services'>Services</section>
   )
 }
 
