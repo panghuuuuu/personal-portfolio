3d0de4f6518e6fedb57fc2d81fba64a07ba61242
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-04-05 23:01:44 +0800

    fixed works modals and container

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,30 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-const MODAL_STYLE = {
-  position: "fixed",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  backgroundColor: "black",
-  padding: "50px",
-  zIndex: 1000,
-};
-const OVERLAY_STYLE = {
-  position: "fixed",
-  top: 0,
-  bottom: 0,
-  right: 0,
-  left: 0,
-  backgroundColor: "rgba(0, 0, 0, 0.7)",
-};
+import "../stylesheets/modals.css";
+import { RxCross2 } from "react-icons/rx";
+
 export default function Modal({ isOpen, children, onClose }) {
   if (!isOpen) return null;
 
   return ReactDOM.createPortal(
     <>
-      <div style={OVERLAY_STYLE} />
-      <div style={MODAL_STYLE}>
-        <button onClick={onClose}>Close Modal</button>
+      <div className="overlay__container" />
+      <div className="modal__container">
+        <button className="close-btn" onClick={onClose}>
+          <RxCross2 />
+        </button>
         {children}
       </div>
     </>,
