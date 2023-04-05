371c3ff910c80404f13ad82db6e1239605066a1f
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-04-05 08:59:24 +0800

    finalized portfolio layout

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,21 +1,68 @@
 import React, { useState } from "react";
 import Modal from "./Modals";
 import "../stylesheets/works.css";
-
-const BUTTON_WRAPPER_STYLES = {
-  position: "relative",
-  zIndex: 1,
-};
+import { Projects } from "../constants/projects";
 
 const Works = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedProj, setSelectedProj] = useState("");
+  const works = [];
+  let mainTitle = "";
+  let main = {};
+  let mainTech = [];
+  Object.entries(Projects).forEach(([key, value], i) => {
+    let techContent = [];
+    value.technologies.forEach((item) => {
+      techContent.push(<span>{item} </span>);
+    });
+
+    if (i !== 0) {
+      works.push(
+        <div className="indiv__project">
+          <img
+            src={value.image}
+            alt={key}
+            onClick={() => {
+              setSelectedProj(key);
+              setIsOpen(true);
+            }}
+          />
+          <h2> {key} </h2>
+          <div className="tech_items">{techContent}</div>
+        </div>
+      );
+    } else {
+      mainTitle = key;
+      main = Projects[mainTitle];
+      mainTech = techContent;
+    }
+  });
   return (
     <section id="works">
-      <div className={BUTTON_WRAPPER_STYLES}>
-        <button onClick={() => setIsOpen(true)}>Open Modal</button>
+      <div className="container works__container">
+        <div className="projects">
+          <div className="main__proj">
+            <img
+              src={main.image}
+              alt="main"
+              onClick={() => {
+                setSelectedProj(mainTitle);
+                setIsOpen(true);
+              }}
+            />
+            <h1>{mainTitle}</h1>
+            <div className="tech_items">{mainTech}</div>
+          </div>
+          <div className="sub__projects">{works}</div>
+        </div>
         <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-          {" "}
-          Fancy Modal
+          {selectedProj && (
+            <>
+              <h1>{selectedProj}</h1>
+              <img src={Projects[selectedProj].image} alt={selectedProj} />
+              <p>{Projects[selectedProj].description}</p>
+            </>
+          )}
         </Modal>
       </div>
     </section>
