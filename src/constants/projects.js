4436766af102078e9a9231a0c912087d23c02369
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-04-05 08:59:24 +0800

    finalized portfolio layout

---
+++ src/constants/projects.js
@@ -0,0 +1,48 @@
+import Main from "../assets/works/1.png";
+import Second from "../assets/works/2.jpeg";
+import Third from "../assets/works/3.jpeg";
+import Fourth from "../assets/works/4.png";
+import Fifth from "../assets/works/5.jpeg";
+
+export const Projects = {
+  "Project 1": {
+    image: Main,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    github_link: "https://github.com/",
+    live_site: "https://",
+    technologies: ["Next.js", "Tailwind", "Firebase"],
+  },
+  "Project 2": {
+    image: Second,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    github_link: "https://github.com/",
+    live_site: "https://",
+    technologies: ["Node", "Express", "Firebase", "Vue"],
+  },
+  "Project 3": {
+    image: Third,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    github_link: "https://github.com/",
+    live_site: "https://",
+    technologies: ["Flutter"],
+  },
+  "Project 4": {
+    image: Fourth,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    github_link: "https://github.com/",
+    live_site: "https://",
+    technologies: ["React", "Redux", "Heroko"],
+  },
+  "Project 5": {
+    image: Fifth,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    github_link: "https://github.com/",
+    live_site: "https://",
+    technologies: ["React", "Redux", "Heroko"],
+  },
+};
