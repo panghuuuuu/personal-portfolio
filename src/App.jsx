d788bc33f864f372d2cd576619dda7033e2d9dda
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-07-01 22:37:22 +0800

    fixed footer and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Works from "./components/Works";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
+      <Nav />
       <Header />
       <Experience />
       <Skills />
       <Works />
       <Contact />
+      <Footer />
     </div>
   );
 };
