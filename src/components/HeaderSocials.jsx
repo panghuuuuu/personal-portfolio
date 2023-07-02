5258c6e510647a24034d1acccc3e564b7df67287
Ysabella Panghulan <ysabella.panghulan@obf.ateneo.edu>
2023-07-02 21:24:22 +0800

    updated favicon

--- src/components/HeaderSocials.jsx
+++ src/components/HeaderSocials.jsx
@@ -8,7 +8,7 @@ import { HiMail } from "react-icons/hi";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://gitlab.com/panghuuuuu" rel="noreferrer" target="_blank">
+      <a href="https://github.com/panghuuuuu" rel="noreferrer" target="_blank">
         <BsGithub />
       </a>
       <a href="https://gitlab.com/panghuuuuu" rel="noreferrer" target="_blank">
